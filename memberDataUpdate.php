0b42ffa5273f0d9d54bdb38a925657986f2b5ae4
105021113 <zoozootom@gmail.com>
2024-02-06 14:41:50 +0800

    fix member data update

--- memberDataUpdate.php
+++ memberDataUpdate.php
@@ -8,9 +8,10 @@ try {
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 	require_once("connect_chd104g2.php");
 	//建立sql指令
-    $sql = "update member set member_name=:member_name";
+    $sql = "update member set status=:member_status where member_no=:member_no";
     $member = $pdo->prepare($sql);
-    $member->bindValue(":member_name", $memberData["member_name"]);
+    $member->bindValue(":member_no", $memberData["member_no"]);
+    $member->bindValue(":member_status", $memberData["status"]);
     $member->execute();
 
 	$result = ["error" => false, "msg" => "成功修改會員資料"];
