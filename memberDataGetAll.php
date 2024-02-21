5ed3bb1525b3e5f33d80f6b65a5a1f3f06706a53
105021113 <zoozootom@gmail.com>
2024-02-21 18:48:19 +0800

    修怪東西

--- memberDataGetAll.php
+++ memberDataGetAll.php
@@ -1,12 +1,6 @@
 <?php
-$memberData = json_decode(file_get_contents("php://input"), true);
-
-$email = $_POST['email'];
-$psw = $_POST['psw'];
-
 ini_set("display_errors", "On");
 try {
-
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
@@ -15,21 +9,15 @@ try {
     }else{
         // require_once("https://tibamef2e.com/chd104/g2/php/connect_chd104g2.php");
         require_once("connect_chd104g2.php");
-
     }
     //建立sql指令
-    $sql = "select * from member where email = :member_email AND password = :member_psw";
-    $members = $pdo->query($sql);
-
-    $member->bindValue(":member_email", $email);
-    $member->bindValue(":member_psw", $psw);
-    $member->execute();
-
-    $memberRows = $members->fetchAll(PDO::FETCH_ASSOC);
-
+    $sql = "select * from member";
+    $member = $pdo->query($sql);
+    $memberRows = $member->fetchAll(PDO::FETCH_ASSOC);
     $result = ["error" => false, "msg" => "", "members" => $memberRows];
 } catch (PDOException $e) {
     $result = ["error" => true, "msg" => $e->getMessage()];
     // echo "系統暫時不能正常運行，請稍後再試<br>";	
 }
 echo json_encode($result);
+?>
